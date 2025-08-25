13d571bda2c32b2003bade34e50601d25d0cd81f
Máté Vass <vass.mate99@gmail.com>
2025-08-25 14:26:00 +0200

    exact match deduplicate argparse

--- exact_match_deduplicate.py
+++ exact_match_deduplicate.py
@@ -1,130 +1,105 @@
-#%%
+import os
+import argparse
 import json
 from rapidfuzz import fuzz, process
-#%%
-small = "mcu"
-big = "marvel"
-#%%
-mappings_file_small = "./_input/mappings/" + small + ".json"
-mappings_file_big = "./_input/mappings/" + big + ".json"
 
-exact_match_file = "./_input/exact_match_all/" + small + "-" + big + ".json"
-exact_match_deduplicated_file = "./_input/exact_match_deduplicated/" + small + "-" + big + ".json"
-#%%
-with open(mappings_file_small) as file:
-    mappings_small = {str(v): k for k, v in json.load(file).items()}
-    mappings_small_reversed = {v: k for k, v in mappings_small.items()}
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="Input folder")
+parser.add_argument("-o", "--output", help="Output folder")
+args = parser.parse_args()
 
-with open(mappings_file_big) as file:
-    mappings_big = {str(v): k for k, v in json.load(file).items()}
-    mappings_big_reversed = {v: k for k, v in mappings_big.items()}
+if not os.path.exists(args.output):
+    os.makedirs(args.output)
 
-with open(exact_match_file) as emf:
-    exact_match = json.load(emf)
-#%%
-len(exact_match)
-#%%
-keep_exact_match = list()
-#%%
-exact_match_rightleft_dict = dict()
+files = os.listdir(args.input)
 
-for em in exact_match:
-    if em[1] in exact_match_rightleft_dict:
-        exact_match_rightleft_dict[em[1]].append(em[0])
-    else:
-        exact_match_rightleft_dict[em[1]] = [em[0]]
-#%%
-count_rightleft_duplicates = 0
+for file in files:
 
-for k, v in exact_match_rightleft_dict.items():
-    if len(v) > 1:
-        count_rightleft_duplicates += 1
-#%%
-print("Rightleft Single:", len(exact_match_rightleft_dict) - count_rightleft_duplicates)
-print("Rightleft Multiple:",  count_rightleft_duplicates)
-#%%
-len(exact_match_rightleft_dict)
-#%%
-for k, v in exact_match_rightleft_dict.items():
-    if len(v) > 1:
-        type_right = k.split("/")[4]
-        keep_left = [item for item in v if item.split("/")[4] == type_right]
-        if len(keep_left) > 0:
-            best_match, score, _ = process.extractOne(k, keep_left, scorer=fuzz.ratio)
-            keep_exact_match.append([best_match, k])
-    else:
-        keep_exact_match.append([v[0], k])
-#%%
-len(keep_exact_match)
-#%%
-exact_match_leftright_dict = dict()
+    filename, extension = os.path.splitext(file)
+    input_path = os.path.join(args.input, file)
+    output_path = os.path.join(args.output, filename + ".json")
 
-for kem in keep_exact_match:
+    small = filename.split("-")[0]
+    big = filename.split("-")[1]
 
-    if kem[0] in exact_match_leftright_dict:
-        exact_match_leftright_dict[kem[0]].append(kem[1])
-    else:
-        exact_match_leftright_dict[kem[0]] = [kem[1]]
-#%%
-count_leftright_duplicates = 0
+    with open(input_path) as emf:
+        exact_match = json.load(emf)
 
-for k, v in exact_match_leftright_dict.items():
-    if len(v) > 1:
-        count_leftright_duplicates += 1
-#%%
-print("Leftright Single:", len(exact_match_leftright_dict) - count_leftright_duplicates)
-print("Leftright Multiple:",  count_leftright_duplicates)
-#%%
-len(exact_match_leftright_dict)
-#%%
-keep_exact_match = list()
-#%%
-for k, v in exact_match_leftright_dict.items():
-    if len(v) > 1:
-        type_left = k.split("/")[4]
-        keep_right = [item for item in v if item.split("/")[4] == type_left]
-        if len(keep_right) > 0:
-            best_match, score, _ = process.extractOne(k, keep_right, scorer=fuzz.ratio)
-            keep_exact_match.append([k, best_match])
-    else:
-        keep_exact_match.append([k, v[0]])
-#%%
-len(keep_exact_match)
-#%%
-exact_match_leftright_dict = dict()
-exact_match_rightleft_dict = dict()
+    keep_exact_match = list()
 
-for em in keep_exact_match:
+    exact_match_rightleft_dict = dict()
 
-    if em[0] in exact_match_leftright_dict:
-        exact_match_leftright_dict[em[0]].append(em[1])
-    else:
-        exact_match_leftright_dict[em[0]] = [em[1]]
+    for em in exact_match:
+        if em[1] in exact_match_rightleft_dict:
+            exact_match_rightleft_dict[em[1]].append(em[0])
+        else:
+            exact_match_rightleft_dict[em[1]] = [em[0]]
 
-    if em[1] in exact_match_rightleft_dict:
-        exact_match_rightleft_dict[em[1]].append(em[0])
-    else:
-        exact_match_rightleft_dict[em[1]] = [em[0]]
-#%%
-count_leftright_duplicates = 0
-count_rightleft_duplicates = 0
+    count_rightleft_duplicates = 0
 
-for k, v in exact_match_leftright_dict.items():
-    if len(v) > 1:
-        count_leftright_duplicates += 1
+    for k, v in exact_match_rightleft_dict.items():
+        if len(v) > 1:
+            count_rightleft_duplicates += 1
 
-for k, v in exact_match_rightleft_dict.items():
-    if len(v) > 1:
-        count_rightleft_duplicates += 1
-#%%
-len(keep_exact_match)
-#%%
-print("Leftright Single:", len(keep_exact_match) - count_leftright_duplicates)
-print("Leftright Multiple:",  count_leftright_duplicates)
-print()
-print("Rightleft Single:", len(keep_exact_match) - count_rightleft_duplicates)
-print("Rightleft Multiple:",  count_rightleft_duplicates)
-#%%
-with open(exact_match_deduplicated_file, "w") as f:
-    json.dump(keep_exact_match, f, indent=4)
-#%%
+    for k, v in exact_match_rightleft_dict.items():
+        if len(v) > 1:
+            type_right = k.split("/")[4]
+            keep_left = [item for item in v if item.split("/")[4] == type_right]
+            if len(keep_left) > 0:
+                best_match, score, _ = process.extractOne(k, keep_left, scorer=fuzz.ratio)
+                keep_exact_match.append([best_match, k])
+        else:
+            keep_exact_match.append([v[0], k])
+
+    exact_match_leftright_dict = dict()
+
+    for kem in keep_exact_match:
+        if kem[0] in exact_match_leftright_dict:
+            exact_match_leftright_dict[kem[0]].append(kem[1])
+        else:
+            exact_match_leftright_dict[kem[0]] = [kem[1]]
+
+    count_leftright_duplicates = 0
+
+    for k, v in exact_match_leftright_dict.items():
+        if len(v) > 1:
+            count_leftright_duplicates += 1
+
+    keep_exact_match = list()
+
+    for k, v in exact_match_leftright_dict.items():
+        if len(v) > 1:
+            type_left = k.split("/")[4]
+            keep_right = [item for item in v if item.split("/")[4] == type_left]
+            if len(keep_right) > 0:
+                best_match, score, _ = process.extractOne(k, keep_right, scorer=fuzz.ratio)
+                keep_exact_match.append([k, best_match])
+        else:
+            keep_exact_match.append([k, v[0]])
+
+    exact_match_leftright_dict = dict()
+    exact_match_rightleft_dict = dict()
+
+    for em in keep_exact_match:
+        if em[0] in exact_match_leftright_dict:
+            exact_match_leftright_dict[em[0]].append(em[1])
+        else:
+            exact_match_leftright_dict[em[0]] = [em[1]]
+        if em[1] in exact_match_rightleft_dict:
+            exact_match_rightleft_dict[em[1]].append(em[0])
+        else:
+            exact_match_rightleft_dict[em[1]] = [em[0]]
+
+    count_leftright_duplicates = 0
+    count_rightleft_duplicates = 0
+
+    for k, v in exact_match_leftright_dict.items():
+        if len(v) > 1:
+            count_leftright_duplicates += 1
+
+    for k, v in exact_match_rightleft_dict.items():
+        if len(v) > 1:
+            count_rightleft_duplicates += 1
+
+    with open(output_path, "w") as f:
+        json.dump(keep_exact_match, f, indent=4)
